8526da142cdd5b57f1236e001288ab4110092b1e
Wahyu Pratomo <wahyupratomo187@gmail.com>
2025-07-06 13:44:25 +0000

    aku ga mau ada warna selain warna biru di tombol linkedin

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,6 +19,8 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        linkedin:
+          "bg-gradient-to-r from-[#0077b5] to-[#00a0dc] text-white hover:shadow-lg focus-visible:ring-blue-500",
       },
       size: {
         default: "h-10 px-4 py-2",
